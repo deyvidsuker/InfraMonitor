2f6671fae1d7e84e9471dd6678e259c7fe48bf17
deyvidsuker <100437690+deyvidsuker@users.noreply.github.com>
2026-06-18 12:28:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log("InfraMonitor iniciado");
+async function cargarDatos() {
+
+    try {
+
+        const respuesta =
+            await fetch("http://172.20.44.193:3000/status");
+
+        const datos = await respuesta.json();
+
+        document.getElementById("cpu").innerText =
+            datos.cpu;
+
+        document.getElementById("ram").innerText =
+            datos.ram;
+
+        document.getElementById("disk").innerText =
+            datos.disk;
+
+    } catch (error) {
+
+        console.error(error);
+
+    }
+
+}
+
+cargarDatos();
+
+setInterval(cargarDatos, 5000);
